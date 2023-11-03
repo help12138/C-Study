f498a96760c5b3a83ff16d2e2a63ee1a4f08d08c
help12138 <18736993700@163.com>
2023-11-03 16:31:53 +0800

    类型判定

--- CDemo/test.cpp
+++ CDemo/test.cpp
@@ -14,11 +14,34 @@ void pointer(void) {
 	*/
 	int b = 20;
 	int* const num2 = &b; // num2是一个指针常量，指向b，但不能通过num2修改num2的指向，可以通过num2修改b的值
-
+	/**
+	* Tips; 如果要阅读一个类型，可以从右往左阅读理解
+	* 例如 const int * const arr = 0  
+	* 读作  一个常量类型的指针指向一个int类型的常量
+	*/
 	
 }
 
+
+void typeInfo(void) {
+	/**
+	* 类型别名 
+	* 旧方法是 typedef
+	* 新方法为 using
+	*/
+	typedef double wages;
+	using SI = int;
+
+	/**
+	* auto类型
+	* 根据初始值来推算变量的类型，使用auto定义的变量必须要有初始值
+	*/
+	int aa = 14, bb = 12;
+	auto sum = aa + bb;
+}
+
 void main(void) {
-	pointer();
+	//pointer();
+	
 	return;
 }
